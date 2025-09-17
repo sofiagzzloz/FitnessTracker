6fb12fc96761e16ef15c1cca2855904dff1ab99f
sofiagzzloz <sofiagzzloz@gmail.com>
2025-09-17 10:14:39 +0200

    db configured correctly

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from .db import init_db
 
 app = FastAPI(title= "Fitness Tracker")
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
 @app.get("/")
 def read_root():
